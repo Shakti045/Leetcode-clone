9c6ef697642db295f7132007a0112f3aeafcfc3b
Shakti045 <shaktimandal045@gmail.com>
2024-02-12 21:45:07 +0530

    added

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
     publicRoutes:['/','/login','/signup','/forgot-password','/reset-password','/not-found'],
     ignoredRoutes:[
-      '/api/*',
+      '/api/:path*',
     ],
 });
  
